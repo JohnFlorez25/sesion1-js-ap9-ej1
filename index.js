710421ca955dce42110681fdfb3c46ccd21be870
John Faber Flórez <john.florez25@gmail.com>
2021-11-05 16:43:29 -0500

    Algoritmo para cocinar un huevo

---
+++ index.js
@@ -0,0 +1,26 @@
+//ALGORITMO IMPLEMENTADO EN JAVASCRIPT PARA COCINAR UN HUEVO
+/**
+ * Autor: John Faber Flórez Vasco
+ * Fecha: 11/05/2021
+ * correo: john.florez@academiageek.co
+ */
+
+//ENTRADAS
+let nombre = "John Flórez";
+let frito = true;
+let salado = false;
+//PROCESOS
+console.log("Bienvenido " + nombre + " a cocinar a tu huevo");
+if(frito == true){
+    console.log("Freir el huevo");
+} else {
+    console.log("Hervir el huevo");
+}
+
+if(salado == true){
+    console.log("Agregar sal");
+} 
+
+//SALIDAS
+
+console.log("Servir en el plato");
